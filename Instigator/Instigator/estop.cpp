8e4bc0a6cf16eece9b20a33f6157507665a12a9b
Will Rolke <will.rolke@gmail.com>
2012-04-04 19:43:42 -0400

    Reset switch one pin 6, port F.

--- Instigator/Instigator/estop.cpp
+++ Instigator/Instigator/estop.cpp
@@ -13,8 +13,8 @@
 static volatile bool estop = false;
 
 void estop_init(){
-	PORTF.DIRSET &=  0x0F;		 //Set pin 7 as input leave pwm pins alone
-	PORTF.INTCTRL	=TC_OVFINTLVL_HI_gc;		// EStop set to High Priority
+	PORTF.DIRSET  &= _BV(7) | _BV(6);		 //Set pin 7 and 6 as input leave pwm pins alone
+	PORTF.INTCTRL  = TC_OVFINTLVL_HI_gc;		// EStop set to High Priority
 	PORTF.PIN7CTRL = PORT_ISC_FALLING_gc | PORT_OPC_PULLUP_gc;		//Set pin 7 to be pulled up and interupt to occur on the falling edge
 	PORTF.INT0MASK = 0x80;  //Set pin 7 in port F to be part of an interrupt
 }
