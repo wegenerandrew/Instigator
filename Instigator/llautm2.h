937bd628860a11f1026dccdfb8f0e1ba23b7ac07
Will Rolke <will.rolke@gmail.com>
2012-05-27 01:13:12 -0400

    Got rid of whitespace.

--- Instigator/llautm2.h
+++ Instigator/llautm2.h
@@ -23,7 +23,7 @@ typedef struct { double xCoordinate; double yCoordinate; }utmCoordinates;
 
 
 utmCoordinates * LLA_UTM2(double lat, double lon)
-{ 
+{
 //Geodetic refrence system constants
 //GRS80
 //m_a_c = 6378137.0; //equitorial radius or semimajor axis of the ellipsode //reference
@@ -69,12 +69,12 @@ double zone = floor(lon/6) + 31;
 if (zone == 61)
     zone = 60;
 
- 
+
 //Find lon0
 lon0 = 6*(zone-30)-3;
- 
+
 lam0 = pi*lon0/180;
- 
+
 // zone = 1;
 // if lon >= 0 //Longitude is in the Eastern hemisphere
 //     zone = 31 + floor(lon/6)
@@ -84,17 +84,17 @@ lam0 = pi*lon0/180;
 // 
 // lon0 = -180 + (zone-1)*6+3
 // lam0 = pi*lon0/180
- 
+
 k0 = 0.9996; //scale on central meridian
 x0 = 500000; //False Easting
- 
+
 //Equations 8-12 through 8-15, pp 1395
 e2p = .0067394967407447;  //e2 / (1 - e2);
 N = a/sqrt(1-e2*pow(sin(phi),2));
 T = pow(tan(phi),2);
 C = e2p*pow(cos(phi),2);
 A = (lam - lam0)*cos(phi);
- 
+
 //M is true distance along central meridian from the Equator to phi.
 //Equ 3-21, pp 1395
 M1 = .99832429845317 * phi;   // (1-e2/4 -3*pow(e2,2)/64 - 5*pow(e2,3)/256)*phi;
@@ -103,12 +103,12 @@ M3 = 2.6390465931431e-6 * sin(4*phi); //(15*pow(e2,2)/256 + 45*pow(e2,3)/1024)*s
 M4 = 3.4180460842809e-9 * sin(6*phi);//(35*pow(e2,3)/3072)*sin(6*phi);
 M = a*( M1 - M2 + M3 - M4 );
 M0 = 0;
- 
+
 //equation 8-9, pp 1395 Note splitting of equation for easier handling
 x1 = A + (1-T+C)*pow(A,3)/6;
 x2 = (5-18*T + pow(T,2)+72*C-.38708908109632)*pow(A,5)/120; //(5-18*T + pow(T,2)+72*C-58*e2p)*pow(A,5)/120;
 x = k0*N*(x1 + x2) + x0;//False easting may be added here
- 
+
 //equation 8-9, pp 1395 Note splitting of equation for easier handling
 y1 = M - M0;
 y2 = pow(A,2)/2 + (5-T+9*C+4*pow(C,2))*pow(A,4)/24;
